1f069c47a3177a857f09644082addab9a36a3178
Unknown <batrakov.vitaly@gmail.com>
2018-07-06 17:15:36 +0700

    Add Feed List
    Add FeedListTableViewController
    Delete ViewController

---
+++ RSSFeedReaderSwift/RSSReaderSwift/PresentationLayer/Modules/FeedList/FeedListTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FeedListTableViewController.swift
+//  RSSReaderSwift
+//
+//  Created by Vitaly Batrakov on 06.07.2018.
+//  Copyright © 2018 vbat. All rights reserved.
+//
+
+import UIKit
+
+class FeedListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+}

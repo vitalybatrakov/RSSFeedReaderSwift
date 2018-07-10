386733e215ee06ad0cc5e35dfc52b985ea2ded38
Unknown <batrakov.vitaly@gmail.com>
2018-07-10 17:34:09 +0700

    Refactor feed service

--- RSSFeedReaderSwift/RSSReaderSwift/Services/FeedServiceImpl.swift
+++ RSSFeedReaderSwift/RSSReaderSwift/Services/FeedServiceImpl.swift
@@ -12,19 +12,32 @@ import FeedKit
 class FeedServiceImpl: FeedService {
     
     func getFeeds(with complition: @escaping ([Feed]) -> Void) {
-        let feedURL = URL(string: "https://habrahabr.ru/rss/interesting/")!//"http://images.apple.com/main/rss/hotnews/hotnews.rss")!
+        let feedURL = URL(string: "https://habrahabr.ru/rss/interesting/")!
         let parser = FeedParser(URL: feedURL)
         parser?.parseAsync(queue: DispatchQueue.global(qos: .default)) { (result) in
             guard let feed = result.rssFeed, result.isSuccess else {
                 print(result.error as Any)
                 return
             }
-            DispatchQueue.main.async {
-                let feedItems = feed.items?.map {
-                    FeedItem(title: $0.title!, body: $0.description!, link: $0.link!)
-                }
-                complition([Feed(title: feed.title!, items: feedItems!)])
-            }
+            self.process(feed: feed, with: complition)
         }
+    }
+    
+    private func process(feed: RSSFeed, with complition: @escaping ([Feed]) -> Void) {
+        DispatchQueue.main.async {
+            guard let feedTitle = feed.title,
+                  let feedItems = self.mapFeedItems(from: feed) else { return }
+            complition([Feed(title: feedTitle, items: feedItems)])
+        }
+    }
+    
+    private func mapFeedItems(from feed: RSSFeed) -> [FeedItem]? {
+        return feed.items?.compactMap({ (item) -> FeedItem? in
+            guard let title = item.title,
+                let description = item.description,
+                let link = item.link else { return nil }
+            
+            return FeedItem(title: title, body: description, link: link)
+        })
     }
 }
